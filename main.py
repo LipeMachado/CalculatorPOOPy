911b8d9b9d0730e0b11de74dd5dd3e7a7846425c
LipeMachado <silvalipe765@gmail.com>
2022-04-17 19:22:36 -0300

    Update

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
+
+# ============================== Importações =================================
+
 from os import system
 from os import sys
 from math import sqrt, log
 
+# ============================== Classes =================================
 
-class Calculadora:
-    def __init__(self):
-        self.limparTela()
-        self.menuOpcoes()
 
-    def limparTela(self):
+class MenuOpcoes():
+
+    def limparTela():
         system("cls || clear")
 
-    def menuOpcoes(self):
+    def tabela():
         print("===================================\n"
               "|            0 - Sair             |\n"
               "|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|\n"
@@ -35,104 +37,131 @@ class Calculadora:
               "|          9 - Logaritmo          |\n"
               "===================================\n")
 
-        opcao = int(input("Informe um número de 0 a 9: "))
 
-        if opcao == 1:
-            primeiroValor = float(input("\nInsira o primeiro valor: "))
-            segundoValor = float(input("Insira o segundo valor : "))
+class Logica():
 
-            print(self.somar(primeiroValor, segundoValor))
-            self.menuOpcoes()
+    def somar(primeiroValor, segundoValor):
+        resultado = primeiroValor + segundoValor
+        return f"Resultado da soma é: {resultado}"
 
-        elif opcao == 2:
-            primeiroValor = float(input("\nInsira o primeiro valor: "))
-            segundoValor = float(input("Insira o segundo valor : "))
+    def subtracao(primeiroValor, segundoValor):
+        resultado = primeiroValor - segundoValor
+        return f"Resultado da subtração é: {resultado}"
 
-            print(self.subtracao(primeiroValor, segundoValor))
-            self.menuOpcoes()
+    def multiplicacao(primeiroValor, segundoValor):
+        resultado = primeiroValor * segundoValor
+        return f"Resultado da multiplicação é: {resultado}"
 
-        elif opcao == 3:
-            primeiroValor = float(input("\nInsira o primeiro valor: "))
-            segundoValor = float(input("Insira o segundo valor : "))
+    def divisao(primeiroValor, segundoValor):
+        resultado = primeiroValor / segundoValor
+        return f"Resultado da divisão é: {resultado}"
 
-            print(self.multiplicacao(primeiroValor, segundoValor))
-            self.menuOpcoes()
+    def potenciacaoQuadratica(valor):
+        resultado = valor ** 2
+        return f"Resultado da potenciação quadratica é: {resultado}"
 
-        elif opcao == 4:
-            primeiroValor = float(input("\nInsira o primeiro valor: "))
-            segundoValor = float(input("Insira o segundo valor : "))
+    def potenciacaoCubica(valor):
+        resultado = valor ** 3
+        return f"Resultado da potenciação ao cubo é: {resultado}"
 
-            print(self.divisao(primeiroValor, segundoValor))
-            self.menuOpcoes()
+    def raizQuadrada(valor):
+        resultado = sqrt(valor)
+        return f"Resultado da potenciação ao cubo é: {resultado}"
 
-        elif opcao == 5:
-            valor = float(input("\nInsira o valor: "))
+    def raizCubica(valor):
+        resultado = pow(valor, 1/3)
+        return f"Resultado da potenciação ao cubo é: {resultado}"
 
-            print(self.potenciacaoQuadratica(valor))
-            self.menuOpcoes()
+    def logaritmo(valor):
+        resultado = log(valor)
+        return f"Resultado da potenciação ao cubo é: {resultado}"
 
-        elif opcao == 6:
-            valor = float(input("\nInsira o valor: "))
 
-            print(self.potenciacaoCubica(valor))
-            self.menuOpcoes()
+class Calculadora():
+    def __init__(self):
+        rodando = True
 
-        elif opcao == 7:
-            valor = float(input("\nInsira o valor: "))
+        while rodando != False:
 
-            print(self.raizQuadrada(valor))
-            self.menuOpcoes()
+            MenuOpcoes.tabela()
+            opcao = int(input("Informe um número de 0 a 9: "))
+            MenuOpcoes.limparTela()
 
-        elif opcao == 8:
-            valor = float(input("\nInsira o valor: "))
+            if opcao == 1:
+                primeiroValor = float(input("\nInsira o primeiro valor: "))
+                segundoValor = float(input("Insira o segundo valor : "))
 
-            print(self.raizCubica(valor))
-            self.menuOpcoes()
+                print("===================================")
+                print(Logica.somar(primeiroValor, segundoValor))
+                print("===================================\n")
 
-        elif opcao == 9:
-            valor = float(input("\nInsira o valor: "))
+            elif opcao == 2:
+                primeiroValor = float(input("\nInsira o primeiro valor: "))
+                segundoValor = float(input("Insira o segundo valor : "))
 
-            print(self.logaritmo(valor))
-            self.menuOpcoes()
+                print("===================================")
+                print(Logica.subtracao(primeiroValor, segundoValor))
+                print("===================================\n")
 
-        elif opcao == 0:
-            sys.exit()
+            elif opcao == 3:
+                primeiroValor = float(input("\nInsira o primeiro valor: "))
+                segundoValor = float(input("Insira o segundo valor : "))
 
-    def somar(self, primeiroValor, segundoValor):
-        self.resultado = primeiroValor + segundoValor
-        return f"Resultado da soma é: {self.resultado}"
+                print("===================================")
+                print(Logica.multiplicacao(primeiroValor, segundoValor))
+                print("===================================\n")
 
-    def subtracao(self, primeiroValor, segundoValor):
-        self.resultado = primeiroValor - segundoValor
-        return f"Resultado da subtração é: {self.resultado}"
+            elif opcao == 4:
+                primeiroValor = float(input("\nInsira o primeiro valor: "))
+                segundoValor = float(input("Insira o segundo valor : "))
 
-    def multiplicacao(self, primeiroValor, segundoValor):
-        self.resultado = primeiroValor * segundoValor
-        return f"Resultado da multiplicação é: {self.resultado}"
+                print("===================================")
+                print(Logica.divisao(primeiroValor, segundoValor))
+                print("===================================\n")
 
-    def divisao(self, primeiroValor, segundoValor):
-        self.resultado = primeiroValor / segundoValor
-        return f"Resultado da divisão é: {self.resultado}"
+            elif opcao == 5:
+                valor = float(input("\nInsira o valor: "))
 
-    def potenciacaoQuadratica(self, valor):
-        self.resultado = valor ** 2
-        return f"Resultado da potenciação quadratica é: {self.resultado}"
+                print("===================================")
+                print(Logica.potenciacaoQuadratica(valor))
+                print("===================================\n")
 
-    def potenciacaoCubica(self, valor):
-        self.resultado = valor ** 3
-        return f"Resultado da potenciação ao cubo é: {self.resultado}"
+            elif opcao == 6:
+                valor = float(input("\nInsira o valor: "))
 
-    def raizQuadrada(self, valor):
-        self.resultado = sqrt(valor)
-        return f"Resultado da potenciação ao cubo é: {self.resultado}"
+                print("===================================")
+                print(Logica.potenciacaoCubica(valor))
+                print("===================================\n")
 
-    def raizCubica(self, valor):
-        self.resultado = pow(valor, 1/3)
-        return f"Resultado da potenciação ao cubo é: {self.resultado}"
+            elif opcao == 7:
+                valor = float(input("\nInsira o valor: "))
 
-    def logaritmo(self, valor):
-        self.resultado = log(valor)
-        return f"Resultado da potenciação ao cubo é: {self.resultado}"
+                print("===================================")
+                print(Logica.raizQuadrada(valor))
+                print("===================================\n")
+
+            elif opcao == 8:
+                valor = float(input("\nInsira o valor: "))
+
+                print("===================================")
+                print(Logica.raizCubica(valor))
+                print("===================================\n")
+
+            elif opcao == 9:
+                valor = float(input("\nInsira o valor: "))
+
+                print("===================================")
+                print(Logica.logaritmo(valor))
+                print("===================================\n")
+
+            elif opcao == 0:
+                sys.exit()
+                rodando = True
+
+
+class Historico():
+    def historico(self):
+        self.teste
 
 
 if __name__ == '__main__':
